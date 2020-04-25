6dc7b1dfe8b76e6bd2b67f403fc146eebc34774d
malajure.santosh@gmail.com <malajure.santosh@gmail.com>
2020-04-25 20:11:24 +0530

    initial setup done to the project

---
+++ Second/Second/src/test/java/com/masm/SecondApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
